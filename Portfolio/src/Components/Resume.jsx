348c730b44ad0eb60d89aa9c5b9ba97e3f867b04
Ethan Flatt <ethanflatt1@gmail.com>
2023-08-11 03:47:44 -0500

    Getting a lot of stuff done here

--- Portfolio/src/Components/Resume.jsx
+++ Portfolio/src/Components/Resume.jsx
@@ -11,7 +11,7 @@ const Resume = () => {
                 <h3>EFL instructor at Gloria English school in Taiwan <span>&nbsp;&nbsp; March 2022 - March 2023</span></h3>
                 <p>Taught around 260 students each week and effectively improved their English speaking ability</p>
                 <p>Communicated effectively with taiwanese coteachers to create fun lesson plans</p>
-                <p>Create many fun and unique games to play to facilitate English development</p>
+                <p>Created many fun and unique games to play to facilitate English development</p>
             </div>
             <div className="education">
                 <h2>Education</h2>
@@ -19,7 +19,7 @@ const Resume = () => {
                 <p>12-week long 500 hours full-time and full-stack web development program conducted in a remote setting, providing experience with the latest front- and back-end programming languages, tools, and methodologies including HTML, CSS, SASS, JavaScript, Node.JS, React.JS, MongoDB, Mongoose, SQL, Python, and Django.
                 Completing in-class projects and personal projects focused on the real-world applications of web development principles and best practices.</p>
                 <h3>Southern Illinois University of Edwardsville<span>&nbsp;&nbsp; August 2017 - August 2021</span></h3>
-                <p>bachelor of Arts in English, Philosophy</p>
+                <p>Bachelor of Arts in English, Philosophy</p>
             </div>
         </div>
         <div className="aboutFooter">
